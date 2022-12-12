5d91dd20bd6a528045bcbe703a4844f67892e171
Clint Antone Alan <110807686+U071CXA@users.noreply.github.com>
2022-12-12 18:36:40 +0800

    Added an input request for YT url and Save directory

--- Downloader.py
+++ Downloader.py
@@ -1,16 +1,6 @@
-#This is a comment in Python
-#First, you need to install a package named 'pytube'
-#   pip3 install pytube
-
-#From pytube, we will be using YouTube
 from pytube import YouTube
-
-#From pytube.cli, we will be using the on_progress method
 from pytube.cli import on_progress
 
-
-#We will define a function that will get the YouTube link and the download destination
-#I set the download_dir to None so this will become optional
 def downloader(video_link, download_dir=None):
     try:
         #I added a progress bar so we know the download progress
@@ -21,7 +11,6 @@ def downloader(video_link, download_dir=None):
         video = tube.streams.filter(progressive=True, file_extension='mp4').first()
         print('FileSize: ' + str(round(video.filesize/(1024*1024))) + 'MB')
 
-        #Let's check if there is a directory provided
         if download_dir is not None:
             video.download(download_dir)
         else:
@@ -32,10 +21,7 @@ def downloader(video_link, download_dir=None):
         print("Error Downloading video: | '" + str(video_link) + "'")
 
 
-#Now let's call the function
-#I will change the download path as C drive will need added authorization
-#I will save the file to the downloads folder
-downloader('https://www.youtube.com/watch?v=qhc_NG17_mA&ab_channel=HisLifeTV','/Users/COLLABERA/Downloads/')
-
-
-#######Thanks!! That's all
+if __name__ == '__main__':
+    yturl = input("YouTube link: ")
+    download_dir = input("Save to: ")       
+    downloader(yturl, download_dir)
